72fb85c5d7e66d5fb2312686f2d3ec220239acec
Animesh Singh <thakuranimeshsingh265@gmail.com>
2025-12-28 14:59:43 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,3 +6,4 @@ import time
 from sklearn.datasets import fetch_california_housing
 
 st.title(' 🏰 House Price prediction using ML')
+st.image('https://img.freepik.com/free-vector/charming-house-with-tree-illustration_1308-176337.jpg?semt=ais_hybrid&w=740&q=80')
