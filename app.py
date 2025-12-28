a83947597573b605d0098dddfef018f7e739c940
Animesh Singh <thakuranimeshsingh265@gmail.com>
2025-12-28 15:38:19 +0530

    Update app.py

--- app.py
+++ app.py
@@ -15,8 +15,11 @@ y = df.iloc[:,-1]
 st.sidebar.title('🏡 Select House features')
 st.sidebar.image('https://img.freepik.com/free-vector/charming-house-with-tree-illustration_1308-176337.jpg?semt=ais_hybrid&w=740&q=80')
 all_value = []
-for i in X:
-   ans = st.sidebar.slider(f'Select {i} value')
+for i in X:  
+   min_value =  int(X[i].min())
+   max_value =  int(X[i].max())
+   ans = st.sidebar.slider(f'Select {i} value',min_value, max_value)
    all_value.append(ans)
 
-st.write(all_value)
+ # st.write(all_value)
+
